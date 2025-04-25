6dc52c4260750d06fef22d902b9a499d1c131cbe
pixelicq <pixelicq@gmail.com>
2025-04-25 22:05:48 +0700

    To Do List 2.0

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-function addUser(){
+function addTodo(){
     let newelement = document.createElement("p");
     newelement.innerHTML = document.getElementById("todo").value;
     let result = document.getElementById("X");
     document.getElementById("todo").value = "";
+    result.appendChild(newelement); 
     
     let newbutton = document.createElement("button");
-    newbutton.innerHTML = "Delete";
+    newbutton.innerHTML = "Delete ❌";
     newbutton.onclick = function() {
         result.removeChild(newelement);
         result.removeChild(newbutton);
     }
     newelement.appendChild(newbutton);
     newbutton.style.marginLeft = "10px";
-    
-    result.appendChild(newelement); 
 }
